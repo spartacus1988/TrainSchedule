7b86197f1704a6a5227ea72126d15f6803d59c72
spartacusxxxxxx <spartacusxxxxxx@gmail.com>
2019-04-10 21:46:17 +0300

    create_neighbors_table was added

--- initializer/cities.py
+++ initializer/cities.py
@@ -5,6 +5,10 @@ class CitiesInitializer:
 	def __init__(self):
 		self.conn = sqlite3.connect("TrainSchedule.db")
 		self.cursor = self.conn.cursor()
+		self.create_cities_table()
+		self.create_neighbors_table()
+
+	def create_cities_table(self):
 		self.cursor.execute("DROP TABLE IF EXISTS cities")
 		self.cursor.execute("""CREATE TABLE IF NOT EXISTS 
 							cities (city_id INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE ON CONFLICT REPLACE, 
@@ -42,3 +46,47 @@ class CitiesInitializer:
 
 		self.cursor.executemany("INSERT INTO cities VALUES(?, ?)", cities)
 		self.conn.commit()
+
+	def create_neighbors_table(self):
+		self.cursor.execute("DROP TABLE IF EXISTS neighbors")
+		self.cursor.execute("""CREATE TABLE IF NOT EXISTS 
+							neighbors (city_id INTEGER PRIMARY KEY UNIQUE ON CONFLICT REPLACE, 
+									   first_neighbor INTEGER NOT NULL,
+									   second_neighbor INTEGER,
+									   third_neighbor INTEGER);""")
+		neighbors = [(1, 11, 2, 12),
+					(2, 1, 3, 4),
+					(3, 2, None, None),
+					(4, 5, 6, None),
+					(5, 4, 2, None),
+					(6, 4, 7, 20),
+					(7, 6, 8, None),
+					(8, 7, 9, None),
+					(9, 8, 10, 18),
+					(10, 9, 11, None),
+					(11, 10, 1, None),
+					(12, 1, 13, None),
+					(13, 12, 14, None),
+					(14, 13, 15, None ),
+					(15, 16, 14, 17),
+					(16, 15, 17, None),
+					(17, 15, 16, None),
+					(18, 19, 9, None),
+					(19, 18, None, None),
+					(20, 21, 6, None),
+					(21, 20, 22, None),
+					(22, 21, 23, None),
+					(23, 24, 29, 22),
+					(24, 25, 23, None),
+					(25, 24, 26, None),
+					(26, 25, None, None),
+					(27, 28, 29, None),
+					(28, 27, 23, 29),
+					(29, 28, 27, None)]
+
+		self.cursor.executemany("INSERT INTO neighbors VALUES(?, ?, ?, ?)", neighbors)
+		self.conn.commit()
+
+
+
+
