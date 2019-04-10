a71f669fb5db74a3b400b41fb955a34ac3a3cde6
spartacusxxxxxx <spartacusxxxxxx@gmail.com>
2019-04-10 17:26:26 +0300

    was created cities table

--- main.py
+++ main.py
@@ -1,44 +1,44 @@
 import sqlite3
 
 conn = sqlite3.connect("TrainSchedule.db")
+cursor = conn.cursor()
 
-# Create table
-cursor.execute("""create table if not exists cities (name text)""")
+# Create table cities
+cursor.execute("DROP TABLE IF EXISTS cities")
+cursor.execute("""CREATE TABLE IF NOT EXISTS 
+	cities (city_id INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE ON CONFLICT REPLACE, 
+			city_name TEXT NOT NULL UNIQUE);""")
+conn.commit()
 
-#cursor.execute("""CREATE TABLE cities
-#                  (name text)
-#               """)
+cities   = [(1, 'Torzhok'),
+			(2, 'Likhoslavl'),
+			(3, 'Tver'),
+			(4, 'Vyshny Volochek'),
+			(5, 'Spirovo'),
+			(6, 'Bologoye'),
+			(7, 'Firovo'),
+			(8, 'Ostashkov'),
+			(9, 'Peno'),
+			(10, 'Selijarovo'),
+			(11, 'Kuvshinovo'),
+			(12, 'Staritsa'),
+			(13, 'Rzhev'),
+			(14, 'Olenino'),
+			(15, 'Nelidovo'),
+			(16, 'Zap Dvina'),
+			(17, 'Zharkovsky'),
+			(18, 'Andreapol'),
+			(19, 'Toropets'),
+			(20, 'Udomlya'),
+			(21, 'Maksatiha'),
+			(22, 'Bezhetsk'),
+			(23, 'Sonkovo'),
+			(24, 'Kashin'),
+			(25, 'Kalyazin'),
+			(26, 'Savyolovo'),
+			(27, 'Sandovo'),
+			(28, 'Krasnyy Kholm'),
+			(29, 'Vesegonsk')]
 
-
-cities = [('Torzhok'),
-          ('Likhoslavl'),
-          ('Tver'),
-          ('Vyshny Volochek'),
-          ('Spirovo'),
-          ('Bologoye'),
-          ('Firovo'),
-          ('Ostashkov'),
-          ('Peno'),
-          ('Selijarovo'),
-          ('Kuvshinovo'),
-          ('Staritsa'),
-          ('Rzhev'),
-          ('Olenino'),
-          ('Nelidovo'),
-          ('Zap Dvina'),
-          ('Zharkovsky'),
-          ('Andreapol'),
-          ('Toropets'),
-          ('Udomlya'),
-          ('Maksatiha'),
-          ('Bezhetsk'),
-          ('Sonkovo'),
-          ('Kashin'),
-          ('Kalyazin'),
-          ('Savyolovo'),
-          ('Sandovo'),
-          ('Krasnyy Kholm'),
-          ('Vesegonsk')]
- 
-cursor.executemany("INSERT INTO albums VALUES (?,?,?,?,?)", albums)
+cursor.executemany("INSERT INTO cities VALUES(?, ?)", cities)
 conn.commit()
