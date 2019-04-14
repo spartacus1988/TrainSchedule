5a9eb5ff91fa487201123b99712e421a6d8489dd
spartacusxxxxxx <spartacusxxxxxx@gmail.com>
2019-04-14 19:53:26 +0300

    gui was put into separate package

---
+++ gui/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+__author__ = 'spartacusxxxxxx'
+__contact__ = 'mixxxxxkot@gmail.com'
+__copyright__ = 'Copyright (C). All rights reserved.'
+__credits__ = ['spartacusxxxxxx']
+__license__ = ''
